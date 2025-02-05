2543e7a38478b0bde0408754ce64cc9ea18ad2cc
sc11082 <sc11082@nyu.edu>
2025-02-05 13:01:28 -0500

    Update Lol
    Update

--- sketch.js
+++ sketch.js
@@ -1,15 +1,44 @@
-let mCamera;
 
-function preload() {
-  mCamera = createCapture(VIDEO, { flipped: true });
-  mCamera.hide();
-}
+        let faceMesh;
+        let mCamera;
+        let faces = [];
+        let ladybug;
+        let ladybugIndex = 0;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+        function preload() {
+            faceMesh = ml5.faceMesh(gotFaces);
+            ladybug = loadImage('https://upload.wikimedia.org/wikipedia/commons/6/6e/Ladybird_%28Coccinellidae%29.jpg'); // Placeholder image
+        }
 
-function draw() {
-  background(180, 200, 255);
-  image(mCamera, 0, 0);
-}
+        function setup() {
+            let canvas = createCanvas(windowWidth, windowHeight);
+            canvas.parent(document.body);
+            mCamera = createCapture(VIDEO, { flipped: true });
+            mCamera.size(windowWidth, windowHeight);
+            mCamera.hide();
+            faceMesh.detect(mCamera);
+        }
+
+        function draw() {
+            background(180, 200, 255);
+            image(mCamera, 0, 0, width, height);
+            
+            if (faces.length > 0) {
+                let face = faces[0];
+                if (face.keypoints.length > 0) {
+                    let keypoint = face.keypoints[ladybugIndex % face.keypoints.length];
+                    image(ladybug, keypoint.x - 10, keypoint.y - 10, 20, 20);
+                    ladybugIndex++;
+                }
+            }
+        }
+
+        function gotFaces(results) {
+            faces = results;
+        }
+
+        function windowResized() {
+            resizeCanvas(windowWidth, windowHeight);
+            mCamera.size(windowWidth, windowHeight);
+        }
+
